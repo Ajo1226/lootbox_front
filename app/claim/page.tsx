2cdd60adff9a6351e312e1eccce14d33451f59a0
Hannah Redler <hannahjredler@gmail.com>
2024-06-01 19:46:59 +0200

    ton connect button

--- app/claim/page.tsx
+++ app/claim/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import TonConnectButton from "./TonConnectButton";
+// import TonConnectButton from "./TonConnectButton";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { connector } from "../components/TonConnector";
+import { RecoilRoot } from "recoil";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 export default function TaskCompleted() {
   const [showText, setShowText] = useState(false);
@@ -16,13 +20,22 @@ export default function TaskCompleted() {
       setShowConnectButton(true);
     }, 7000);
   });
+
+  // useEffect(() => {
+  //   connector.restoreConnection();
+  // }, []);
+
   return (
-    <div className="mt-10">
-      <video src="123.mp4" autoPlay muted />
-      <div className="text-center items-center justify-center -mt-10 text-3xl transition ease-in-out delay-150">
-        <span>{showText ? "You gained 5 Tons!" : null}</span>
-        {showConnectButton ? <TonConnectButton /> : null}
-      </div>
-    </div>
+    <RecoilRoot>
+      <TonConnectUIProvider manifestUrl="https://lootfront.netlify.app>/tonconnect-manifest.json">
+        <div className="mt-10">
+          <video src="123.mp4" autoPlay muted />
+          <div className="text-center items-center justify-center -mt-10 text-3xl transition ease-in-out delay-150">
+            <span>{showText ? "You gained 5 Tons!" : null}</span>
+            {showConnectButton ? <TonConnectButton /> : null}
+          </div>
+        </div>
+      </TonConnectUIProvider>
+    </RecoilRoot>
   );
 }
