f81d992a7c3dc8418610b3141f25232eb75a8b79
Hannah Redler <hannahjredler@gmail.com>
2024-06-01 18:31:21 +0200

    Add button

---
+++ app/claim/TonConnectButton.tsx
@@ -0,0 +1,9 @@
+// import { TonConnect } from "@tonconnect/sdk";
+
+export default function TonConnectButton() {
+  return (
+    <button className="text-lg rounded-xl bg-blue px-5 py-2 my-5 transition ease-in-out delay-150">
+      Connect Wallet to Claim!
+    </button>
+  );
+}
--- app/claim/page.tsx
+++ app/claim/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import TonConnectButton from "./TonConnectButton";
 
 export default function TaskCompleted() {
   const [showText, setShowText] = useState(false);
+  const [showConnectButton, setShowConnectButton] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setShowText(true);
     }, 5000);
+
+    setTimeout(() => {
+      setShowConnectButton(true);
+    }, 7000);
   });
   return (
-    <div>
+    <div className="mt-10">
       <video src="123.mp4" autoPlay muted />
-      <div className="text-center items-center justify-center -mt-3 text-xl">
+      <div className="text-center items-center justify-center -mt-10 text-3xl transition ease-in-out delay-150">
         <span>{showText ? "You gained 5 Tons!" : null}</span>
+        {showConnectButton ? <TonConnectButton /> : null}
       </div>
     </div>
   );
