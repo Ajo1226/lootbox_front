e44a5765e1938ebc25d568fc1148ba00328ff9fe
Hannah Redler <hannahjredler@gmail.com>
2024-06-01 19:02:42 +0200

    Metadata and button connections

--- app/claim/TonConnectButton.tsx
+++ app/claim/TonConnectButton.tsx
@@ -1,6 +1,11 @@
-// import { TonConnect } from "@tonconnect/sdk";
+import { connector } from "./TonConnector";
+import { useEffect } from "react";
 
 export default function TonConnectButton() {
+  useEffect(() => {
+    connector.restoreConnection();
+  }, []);
+
   return (
     <button className="text-lg rounded-xl bg-blue px-5 py-2 my-5 transition ease-in-out delay-150">
       Connect Wallet to Claim!
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Lootboxes for airdrop</title>
+        <meta name="description" content="Lootboxes for airdrop"></meta>
+
+        <meta property="og:url" content="https://lootfront.netlify.app"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content="Lootboxes for airdrop"></meta>
+        <meta property="og:description" content="Lootboxes for airdrop"></meta>
+        <meta
+          property="og:image"
+          content="https://telegra.ph/file/f23302cab436c607f8302.png"
+        ></meta>
+
+        <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:domain" content="lootfront.netlify.app"></meta>
+        <meta
+          property="twitter:url"
+          content="https://lootfront.netlify.app"
+        ></meta>
+        <meta name="twitter:title" content="Lootboxes for airdrop"></meta>
+        <meta name="twitter:description" content="Lootboxes for airdrop"></meta>
+        <meta name="twitter:image" content=""></meta>
+      </head>
       <body className={`${inter.className} px-10 `}>{children}</body>
     </html>
   );
