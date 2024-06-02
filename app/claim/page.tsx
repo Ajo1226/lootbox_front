9b2ad9f637020beaece1c357168d353d6a272a19
Hannah Redler <hannahjredler@gmail.com>
2024-06-02 08:59:23 +0200

    change video to gif

--- app/claim/page.tsx
+++ app/claim/page.tsx
@@ -6,7 +6,8 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { connector } from "../components/TonConnector";
 import { RecoilRoot } from "recoil";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
-
+import LootBoxOpening from "@/public/lootbox-opening-gif.gif";
+import Image from "next/image";
 export default function TaskCompleted() {
   const [showText, setShowText] = useState(false);
   const [showConnectButton, setShowConnectButton] = useState(false);
@@ -29,7 +30,8 @@ export default function TaskCompleted() {
     // <RecoilRoot>
     <TonConnectUIProvider manifestUrl="https://lootfront.netlify.app/tonconnect-manifest.json">
       <div className="mt-10 flex flex-col items-center justify-center justify-items-center">
-        <video src="123.mov" autoPlay muted />
+        {/* <video src="l.mov" autoPlay muted /> */}
+        <Image className="" src={LootBoxOpening} alt="loading..." />
         <div className="text-center items-center justify-center  text-2xl transition ease-in-out delay-150 mb-5">
           <span>
             {showText ? "Congratulations! You have unlocked 5 Tons!" : null}
