690a523bcc2af36a0cc79471797f94b864e8d015
Hannah Redler <hannahjredler@gmail.com>
2024-06-02 09:41:44 +0200

    change to vid

--- app/claim/page.tsx
+++ app/claim/page.tsx
@@ -3,8 +3,8 @@
 import { useState, useEffect } from "react";
 // import TonConnectButton from "./TonConnectButton";
 import { TonConnectButton } from "@tonconnect/ui-react";
-import { connector } from "../components/TonConnector";
-import { RecoilRoot } from "recoil";
+// import { connector } from "../components/TonConnector";
+// import { RecoilRoot } from "recoil";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import LootBoxOpening from "@/public/lootbox-opening-optimize.gif";
 import Image from "next/image";
@@ -30,8 +30,8 @@ export default function TaskCompleted() {
     // <RecoilRoot>
     <TonConnectUIProvider manifestUrl="https://lootfront.netlify.app/tonconnect-manifest.json">
       <div className="mt-10 flex flex-col items-center justify-center justify-items-center">
-        {/* <video src="l.mov" autoPlay muted /> */}
-        <Image className="" src={LootBoxOpening} alt="loading..." />
+        <video src="123.mp4" autoPlay muted />
+        {/* <Image className="" src={LootBoxOpening} alt="loading..." /> */}
         <div className="text-center items-center justify-center  text-2xl transition ease-in-out delay-150 mb-5">
           <span>
             {showText ? "Congratulations! You have unlocked 5 Tons!" : null}
